0e3ac4bc51f430ec0fee6f78108921a3fa8fd000
Smith, Jacob <jacob.smith@dolby.com>
2025-11-27 21:14:42 +0000

    Refactor CDSliderView initialization to enhance label handling and value display logic

--- Source/CDSliderView.m
+++ Source/CDSliderView.m
@@ -6,110 +6,66 @@
 // Licensed under GPL-2.
 
 #import "CDSliderView.h"
+#import "CDSlider.h"
 
 IB_DESIGNABLE
 
 @implementation CDSliderView
 
+// Override value getter to read from the actual slider control
+- (double) value {
+    return self.slider.doubleValue;
+}
+
+// Override value setter to update the slider control
+- (void) setValue:(double)value {
+    self.slider.doubleValue = value;
+}
+
 - (void) initView {
     [super initView];
 
-//    NSString *_sliderLabel = @"OPTION_SLIDER_DEFAULT_LABEL".localized;
-//    if (option[@"slider-label"].wasProvided && ![option[@"slider-label"].stringValue isBlank]) {
-//        _sliderLabel = option[@"slider-label"].stringValue;
-//    }
-//    sliderLabel = [[NSTextField alloc] initWithFrame:NSMakeRect(0, 0, cmFrame.size.width, 14.0f)];
-//    [sliderLabel setBezeled:NO];
-//    [sliderLabel setDrawsBackground:NO];
-//    [sliderLabel setEditable:NO];
-//    [sliderLabel setSelectable:NO];
-//    sliderLabel.alignment = NSLeftTextAlignment;
-//    sliderLabel.stringValue = _sliderLabel;
-//    [sliderView addSubview:sliderLabel];
-//
-//    valueLabel = [[NSTextField alloc] initWithFrame:NSMakeRect(0, 0, cmFrame.size.width, 14.0f)];
-//    [valueLabel setBezeled:NO];
-//    [valueLabel setDrawsBackground:NO];
-//    [valueLabel setEditable:NO];
-//    [valueLabel setSelectable:NO];
-//    valueLabel.alignment = NSRightTextAlignment;
-//    valueLabel.font = [NSFont fontWithName:valueLabel.font.fontName size:10.0f];
-//    if (!option[@"always-show-value"].wasProvided) {
-//        [valueLabel setHidden:YES];
-//    }
-//    [sliderView addSubview:valueLabel];
-//
-//    [self.panel.contentView addSubview:sliderView];
-//
-//
-//    self.slider.alwaysShowValue = option[@"always-show-value"].boolValue;
-//    self.slider.delegate = self;
-//    slider.valueLabel = valueLabel;
-//    slider.minValue = min;
-//    slider.maxValue = max;
-//    slider.doubleValue = value;
-//    slider.numberOfTickMarks = ticks;
-//    slider.sticky = option[@"sticky"].boolValue;
-//    [slider setContinuous:YES];
-//    slider.target = self;
-//    slider.action = @selector(sliderChanged);
-//
-//    [self.controlView addSubview:slider];
-//
-//    // Save self.controlView height
-//    CGFloat oldHeight = cmFrame.size.height;
-//    cmFrame = self.controlView.frame;
-//
-//    if (ticks > 0) {
-//        NSView *tickView = [[NSView alloc] initWithFrame:NSMakeRect(0.0f, cmFrame.origin.y - (cmFrame.size.height - oldHeight) - 17.0f, self.panel.frame.size.width, 18.0f)];
-//        tickView.autoresizingMask = NSViewMinYMargin;
-//
-//        NSUInteger count = slider.numberOfTickMarks;
-//        for (NSUInteger i = 0; i < count; i++) {
-//            CGFloat  length=cmFrame.size.width-2*10;
-//            CGFloat  position=floor((count==1)?length/2:i*(length/(count-1)));
-//            NSTextField *tickLabel = [[NSTextField alloc] initWithFrame:NSMakeRect(cmFrame.origin.x + 10.0f + position, 0, 0, 0)];
-//            [tickLabel setBezeled:NO];
-//            [tickLabel setDrawsBackground:NO];
-//            [tickLabel setEditable:NO];
-//            [tickLabel setSelectable:NO];
-//            tickLabel.stringValue = [NSString stringWithFormat:@"%i", (int)[slider tickMarkValueAtIndex:i]];
-//            tickLabel.font = [NSFont fontWithName:tickLabel.font.fontName size:10.0f];
-//            [tickLabel sizeToFit];
-//            // Center the label on the tick
-//            NSPoint labelOrigin = tickLabel.frame.origin;
-//            labelOrigin.x -= floor(tickLabel.frame.size.width / 2.0f);
-//            [tickLabel setFrameOrigin:labelOrigin];
-//            [tickView addSubview:tickLabel];
-//        }
-//        [self.panel.contentView addSubview:tickView];
-//
-//        // Move matrix to make room for tickView
-//        cmOrigin = cmFrame.origin;
-//        cmOrigin.y += tickView.frame.size.height + 4.0f;
-//        [matrix setFrameOrigin:cmOrigin];
-//
-//        // Add the tickView to the panel height
-//        panelSize = self.panel.contentView.frame.size;
-//        panelSize.height += tickView.frame.size.height + 4.0f;
-//        [self.panel setContentSize:panelSize];
-//        [self resize];
-//    }
+    // Get the dialog (which is actually a CDSlider)
+    CDSlider *sliderDialog = (CDSlider *)self.dialog;
+    
+    // Set the label text (or hide if not provided)
+    if (sliderDialog.options[@"slider-label"].wasProvided && ![sliderDialog.options[@"slider-label"].stringValue isBlank]) {
+        self.label.stringValue = sliderDialog.options[@"slider-label"].stringValue;
+    } else {
+        self.label.stringValue = @"";
+    }
+    
+    // Initialize value label
+    self.valueLabel.stringValue = [NSString stringWithFormat:@"%d", (int)self.slider.doubleValue];
+    
+    // Show/hide value label based on option
+    if (sliderDialog.options[@"always-show-value"].boolValue) {
+        self.valueLabel.hidden = NO;
+    } else {
+        self.valueLabel.hidden = YES;
+    }
+    
+    // Set up slider to call sliderChanged when moved
+    self.slider.target = self;
+    self.slider.action = @selector(sliderChanged);
+    self.slider.continuous = YES;
+    
+    // Hide ticks label for now (would need tick label generation code)
+    self.ticksLabel.hidden = YES;
 
     [self sliderChanged];
 }
 
 - (void) sliderChanged {
-//    CDSliderCell *slider = [matrix cellAtRow:0 column:0];
-//    // Update the label
-//    NSString *label = @"";
-//    if (option[@"return-float"].wasProvided) {
-//        label = [NSString stringWithFormat:@"%.2f", slider.doubleValue];
-//    }
-//    else {
-//        label = [NSString stringWithFormat:@"%i", slider.intValue];
-//    }
-//    valueLabel.stringValue = label;
+    // Update the value label when slider moves
+    if (!self.valueLabel.hidden) {
+        CDSlider *sliderDialog = (CDSlider *)self.dialog;
+        if (sliderDialog.options[@"return-float"].boolValue) {
+            self.valueLabel.stringValue = [NSString stringWithFormat:@"%.2f", self.slider.doubleValue];
+        } else {
+            self.valueLabel.stringValue = [NSString stringWithFormat:@"%d", (int)self.slider.doubleValue];
+        }
+    }
 }
 
 @end
